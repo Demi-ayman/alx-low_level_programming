f62a87182e84038cab9c24d764d261939fa11a12
Demi-ayman <demianaayman123431@gmail.com>
2023-07-21 18:05:42 -0700

    My first commit

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,9 +8,9 @@ void print_square(int size)
 {
 	int row, colum;
 
-	for (row = 0; row < size; row++)
+	for (row = 1; row <= size; row++)
 	{
-		for (colum = 0; colum < size; colum++)
+		for (colum = 1; colum <= size; colum++)
 			_putchar(35);
 		_putchar('\n');
 	}
